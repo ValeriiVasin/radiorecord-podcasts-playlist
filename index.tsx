25775554ac189f517793cf3c20411128849efc0a
Valerii Vasin <vvasin@groupon.com>
2018-06-15 14:16:37 +0200

    Add duration and info toggler

--- index.tsx
+++ index.tsx
@@ -1,22 +1,53 @@
 import feed from "./feed.json";
 
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import { render } from "react-dom";
 
-const App = () => {
-  const items = feed.items.map(item => (
-    <li key={item.url}>
-      <a href={item.url} target="_blank">{item.title}</a>
-    </li>
-  ));
-  return (
-    <Fragment>
-      <h1>Radio Record Latest</h1>
-      <ul>
-        {items}
-      </ul>
-    </Fragment>
-  );
-};
+interface AppState {
+  infoShown: {
+    [id: string]: boolean;
+  };
+}
+
+class App extends Component {
+  state: AppState = {
+    infoShown: {}
+  };
+
+  handleInfoClick = (url: string) => {
+    const { infoShown } = this.state;
+    const isOpened = Boolean(infoShown[url]);
+    const nextInfoShown = { ...infoShown, [url]: !isOpened };
+    this.setState({ infoShown: nextInfoShown });
+  };
+
+  shouldShowInfo = (url: string): boolean => Boolean(this.state.infoShown[url]);
+
+  render() {
+    const items = feed.items.map(item => (
+      <li key={item.url}>
+        <a href={item.url} target="_blank">
+          {item.title}
+        </a>
+        <span> / </span>
+        <span>{item.duration}</span>
+        {item.description && [
+          <span> / </span>,
+          <button onClick={() => this.handleInfoClick(item.url)}>
+            toggle info
+          </button>
+        ]}
+        {this.shouldShowInfo(item.url) ? <pre>{item.description}</pre> : null}
+      </li>
+    ));
+
+    return (
+      <Fragment>
+        <h1>Radio Record Latest Podcasts</h1>
+        <ul>{items}</ul>
+      </Fragment>
+    );
+  }
+}
 
 render(<App />, document.getElementById("root"));
