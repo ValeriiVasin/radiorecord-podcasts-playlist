6013d9a97c8805836fb5065d0b2a31c94e7a9d5f
Valerii Vasin <vvasin@groupon.com>
2020-03-09 23:50:03 +0100

    chore: migrate to monorepo

--- packages/functions/src/radiorecord.ts
+++ packages/functions/src/radiorecord.ts
@@ -1,11 +1,11 @@
-import fetch from 'node-fetch';
-import { parseString } from 'xml2js';
-import { FeedResponse, Item } from '../../feed';
+import fetch from "node-fetch";
+import { parseString } from "xml2js";
+import { FeedResponse, Item } from "./feed";
 
 export const getRadioRecordPodcast = async () => {
-  const responseXML = await fetch('http://www.radiorecord.ru/rss.xml').then(
-    response => response.text()
-  );
+  const responseXML = await fetch(
+    "http://www.radiorecord.ru/rss.xml"
+  ).then(response => response.text());
 
   return new Promise<Podcast>((resolve, reject) => {
     parseString(responseXML, (err, json: FeedResponse) => {
@@ -26,7 +26,7 @@ function mapper(response: FeedResponse): Podcast {
     const title = item.title[0];
     const url = item.enclosure[0].$.url;
     const description = item.description[0];
-    const duration = item['itunes:duration'][0];
+    const duration = item["itunes:duration"][0];
     const pubDate = item.pubDate[0];
 
     return {
