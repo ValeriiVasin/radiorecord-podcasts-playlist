f830405c458a9206187d8c186323f57c06bacceb
Valerii Vasin <vvasin@groupon.com>
2020-03-11 03:02:22 +0100

    feat: add player

--- packages/app-svelte/src/app.svelte
+++ packages/app-svelte/src/app.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Player from "./audio.svelte";
+
   async function fetchPodcasts() {
     return fetch("https://itunes-podcasts.firebaseio.com/items.json").then(
       response => response.json()
@@ -9,14 +11,36 @@
   function toggleInfo(url) {
     toggled[url] = !Boolean(toggled[url]);
   }
+
+  let src;
+  let title;
+  function play(_src, _title) {
+    title = _title;
+    src = _src;
+  }
 </script>
+
+<style>
+  :global(body, html) {
+    padding: 0;
+    margin: 0;
+  }
+</style>
 
 {#await fetchPodcasts()}
   <h1>Loading...</h1>
 {:then items}
+  {#if src}
+    <Player {src} {title} autoplay />
+  {/if}
+
   {#each items as podcast}
     <li>
-      <a href={podcast.url} target="_blank">{podcast.title}</a>
+      <a
+        href={podcast.url}
+        on:click|preventDefault={() => play(podcast.url, podcast.title)}>
+        {podcast.title}
+      </a>
       <span>/</span>
       <span>{podcast.duration}</span>
       {#if podcast.description}
