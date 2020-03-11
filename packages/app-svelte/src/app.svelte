8397c241cc3e9762f0fb1db46c37f6cec9e836d8
Valerii Vasin <vvasin@groupon.com>
2020-03-11 01:35:55 +0100

    feat: svelte app

--- packages/app-svelte/src/app.svelte
+++ packages/app-svelte/src/app.svelte
@@ -1,1 +1,31 @@
-<h1>Hello There!</h1>
+<script>
+  async function fetchPodcasts() {
+    return fetch("https://itunes-podcasts.firebaseio.com/items.json").then(
+      response => response.json()
+    );
+  }
+
+  const toggled = {};
+  function toggleInfo(url) {
+    toggled[url] = !Boolean(toggled[url]);
+  }
+</script>
+
+{#await fetchPodcasts()}
+  <h1>Loading...</h1>
+{:then items}
+  {#each items as podcast}
+    <li>
+      <a href={podcast.url} target="_blank">{podcast.title}</a>
+      <span>/</span>
+      <span>{podcast.duration}</span>
+      {#if podcast.description}
+        <span>/</span>
+        <button on:click={() => toggleInfo(podcast.url)}>toggle info</button>
+      {/if}
+      {#if toggled[podcast.url]}
+        <pre>{podcast.description}</pre>
+      {/if}
+    </li>
+  {/each}
+{/await}
