a92cd6e43733e0ff32377077f50ff81b5aa286d0
Valerii Vasin <valerii.vasin@gmail.com>
2020-10-10 16:27:32 +0200

    feat: play music via audio player

--- packages/app-svelte/src/app.svelte
+++ packages/app-svelte/src/app.svelte
@@ -1,31 +1,40 @@
 <script>
   async function fetchPodcasts() {
-    return fetch("https://itunes-podcasts.firebaseio.com/items.json").then(
-      response => response.json()
-    );
+    return fetch(
+      'https://itunes-podcasts.firebaseio.com/items.json'
+    ).then((response) => response.json());
   }
 
   const toggled = {};
   function toggleInfo(url) {
     toggled[url] = !Boolean(toggled[url]);
   }
+
+  let src = null;
 </script>
 
 {#await fetchPodcasts()}
   <h1>Loading...</h1>
 {:then items}
-  {#each items as podcast}
-    <li>
-      <a href={podcast.url} target="blank">{podcast.title}</a>
-      <span>/</span>
-      <span>{podcast.duration}</span>
-      {#if podcast.description}
+  <!-- svelte-ignore a11y-media-has-caption -->
+  {#if src}<audio {src} controls autoplay />{/if}
+  <ul>
+    {#each items as podcast}
+      <li>
+        <!-- svelte-ignore a11y-invalid-attribute -->
+        <a
+          href="javascript:void(0)"
+          on:click={() => (src = podcast.url)}>{podcast.title}</a>
         <span>/</span>
-        <button on:click={() => toggleInfo(podcast.url)}>toggle info</button>
-      {/if}
-      {#if toggled[podcast.url]}
-        <pre>{podcast.description}</pre>
-      {/if}
-    </li>
-  {/each}
+        <span>{podcast.duration}</span>
+        {#if podcast.description}
+          <span>/</span>
+          <button on:click={() => toggleInfo(podcast.url)}>toggle info</button>
+        {/if}
+        {#if toggled[podcast.url]}
+          <pre>{podcast.description}</pre>
+        {/if}
+      </li>
+    {/each}
+  </ul>
 {/await}
