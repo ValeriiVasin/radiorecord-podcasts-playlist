cc335d246099e724965a05fe3befeb85eead3fa1
Valerii Vasin <valerii.vasin@gmail.com>
2020-10-10 17:23:59 +0200

    feat: player

--- packages/app-svelte/src/app.svelte
+++ packages/app-svelte/src/app.svelte
@@ -11,20 +11,61 @@
   }
 
   let src = null;
+  let title = '';
+  function play(podcast) {
+    src = podcast.url;
+    title = podcast.title;
+  }
 </script>
+
+<style>
+  .player {
+    position: fixed;
+    bottom: 0;
+    background-color: white;
+    width: 100%;
+    border-top: 1px solid #a5a8ab;
+    box-shadow: 0 -2px 8px #a5a8ab;
+  }
+  .title {
+    font-weight: bold;
+    padding: 8px;
+    text-align: center;
+  }
+  audio {
+    width: 100%;
+    padding: 0;
+    margin: 0;
+  }
+
+  .play-button {
+    padding: 0;
+    border: none;
+    background-color: transparent;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  .play-button:hover {
+    color: blue;
+  }
+</style>
 
 {#await fetchPodcasts()}
   <h1>Loading...</h1>
 {:then items}
   <!-- svelte-ignore a11y-media-has-caption -->
-  {#if src}<audio {src} controls autoplay />{/if}
+  {#if src}
+  <div class="player">
+    <div class="title">{title}</div>
+    <audio {src} controls autoplay />
+  </div>
+  {/if}
   <ul>
     {#each items as podcast}
       <li>
-        <!-- svelte-ignore a11y-invalid-attribute -->
-        <a
-          href="javascript:void(0)"
-          on:click={() => (src = podcast.url)}>{podcast.title}</a>
+        <button
+          class="play-button"
+          on:click={() => play(podcast)}>{podcast.title}</button>
         <span>/</span>
         <span>{podcast.duration}</span>
         {#if podcast.description}
